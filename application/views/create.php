34f7932dcff30a5d0eb446780f1f5c75adcdaf3e
ranjeet <1705213032@ietlucknow.ac.in>
2020-03-27 07:48:48 +0530

    bootstrap is added and working

--- application/views/create.php
+++ application/views/create.php
@@ -1,1 +1,12 @@
-create
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD App(Create user)</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assests/css/bootstrap.min.css';?>">
+</head>
+<body>
+    
+</body>
+</html>
